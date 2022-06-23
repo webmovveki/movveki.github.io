7741071f55d796d07e52360f6336d411dcb61b63
Chiararedeffe <84141676+chiararedeffe@users.noreply.github.com>
2022-06-23 18:50:48 +0200

    Update array.js

---
+++ array.js
@@ -0,0 +1,51 @@
+//film
+var fangry = ["Birdbox","Nightmare Alley","The Neon Demon"];
+var fdisgust = ["Il Gladiatore","Les Miserables","Marie Antoinette"];
+var ffear = ["It","La Madre","Lights Out"];
+var fhappy = ["Attenti a quelle due","Festa col morto","Non è romantico"];
+var fneutral = ["10 cose che odio di te","Call me by your name","Lei"];
+var fsad = ["Giovani ribelli","Ragazze interrotte","Velvet buzzsaw"];
+var fsurprise = ["Avatar","Harry Potter","The Hobbit"];
+
+//cibo
+var cangry = ["Dark cookies","Monster Energy","Pumkin spice latte"];
+var cdisgust = ["Birra","Club sandwich","Patatine"];
+var cfear = ["Bloody Mary","Candy corn","Pumkin Pie"];
+var chappy = ["Cioccolata calda","Pancake","Waffle"];
+var cneutral = ["Cosmopolitan","Marshmellow",">ucchero filato"];
+var csad = ["Cioccolata calda","Pancake","Waffle"];
+var csurprise = ["Gelato","Succo","Torta cioccolato"];
+
+//libri
+var langry = ["La psichiatra","Mindhunter","Rosemary's baby"];
+var ldisgust = ["Guerra e Pace","Notre Dame","Piccole donne"];
+var lfear = ["Lovecraft","Poe","Salems lot"];
+var lhappy = ["Bridget Jones","I love shopping con mia sorella","I love shopping"];
+var lneutral = ["Anna Karenina","Cime tempestose","Orgoglio e pregiudizio"];
+var lsad = ["Dorian Gray","Dracula","Frankenstein"];
+var lsurprise = ["Alice nel Paese delle meraviglie","Cronache del Mondo emerso","The Witcher"];
+
+//musica
+var mangry = ["Derek f","ic3 peak","Jozef Van Wissem"];
+var mdisgust = ["Falls of glory","Ivan Torrent","Peter Grundy"];
+var mfear = ["Marilyn Manson","Peter Grundy","Rob Zombie"];
+var mhappy = ["Caleb Hyles","Ed Sheeran","Michael Bublè"];
+var mneutral = ["Ed Sheeran","Michael Bublè","Ultimo"];
+var msad = ["ic3 peak","Jozef Van Wissem","Peter Grundy"];
+var msurprise = ["Falls of Glory","Ivan Torrent","Sham Stalin"];
+
+//serie tv
+var sangry = ["Hannibal","Ratched","You"];
+var sdisgust = ["Gli ultimi Zar","I Medici","Versailles"];
+var sfear = ["American Horror Story","Hill house","Marianne"];
+var shappy = ["Big Bang Theory","Friends","Two broke girls"];
+var sneutral = ["Bridgerton","Le fate ignoranti","Nana"];
+var ssad = ["13","Squid Game","The end of the fucking world"];
+var ssurprise = ["Locke e Key","Penny dreadful","Shadowhunters"];
+
+
+var film = Math.floor(Math.random() * 3);
+var cibo = Math.floor(Math.random() * 3);
+var libri = Math.floor(Math.random() * 3);
+var musica = Math.floor(Math.random() * 3);
+var serietv = Math.floor(Math.random() * 3);
