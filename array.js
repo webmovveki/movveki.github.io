a0e3a1001f20842022a7cf16f58ffeccef3948af
Chiararedeffe <84141676+chiararedeffe@users.noreply.github.com>
2022-06-28 13:15:24 +0200

    risultati

--- array.js
+++ array.js
@@ -51,28 +51,69 @@ var musica = Math.floor(Math.random() * 3);
 var serietv = Math.floor(Math.random() * 3);
 
 var emozione = sessionStorage.getItem("emozione");
-var emozione= emozione[0]
+var emozione= emozione[0];
+console.log(emozione);
 
 
 switch(emozione) {
     case 'a':
+        document.getElementById("filmTxt").innerHTML= fangry[film];
+        document.getElementById("ciboTxt").innerHTML= cangry[cibo];
+        document.getElementById("libriTxt").innerHTML= langry[libri];
+        document.getElementById("musicaTxt").innerHTML= mangry[musica];
+        document.getElementById("serieTxt").innerHTML= sangry[serietv];
     break;
 
     case 'd' :
+        document.getElementById("filmTxt").innerHTML= fdisgust [film];
+        document.getElementById("ciboTxt").innerHTML= cdisgust [cibo];
+        document.getElementById("libriTxt").innerHTML= ldisgust[libri];
+        document.getElementById("musicaTxt").innerHTML= mdisgust[musica];
+        document.getElementById("serieTxt").innerHTML= sdisgust[serietv];
+    break;
+
+    case 'i' :
+        document.getElementById("filmTxt").innerHTML= ffear[film];
+        document.getElementById("ciboTxt").innerHTML= cfear[cibo];
+        document.getElementById("libriTxt").innerHTML= lfear[libri];
+        document.getElementById("musicaTxt").innerHTML= mfear[musica];
+        document.getElementById("serieTxt").innerHTML= sfear[serietv];
     break;
 
     case 'f' :
-    break;
-
-    case 'h' :
+        document.getElementById("filmTxt").innerHTML= fhappy[film];
+        document.getElementById("ciboTxt").innerHTML= chappy[cibo];
+        document.getElementById("libriTxt").innerHTML= lhappy[libri];
+        document.getElementById("musicaTxt").innerHTML= mhappy[musica];
+        document.getElementById("serieTxt").innerHTML= shappy[serietv];
     break;
 
     case 'n' :
+        document.getElementById("filmTxt").innerHTML= fneutral[film];
+        document.getElementById("ciboTxt").innerHTML= cneutral[cibo];
+        document.getElementById("libriTxt").innerHTML= lneutral[libri];
+        document.getElementById("musicaTxt").innerHTML= mneutral[musica];
+        document.getElementById("serieTxt").innerHTML= sneutral[serietv];
+    break;
+
+    case 't' :
+        document.getElementById("filmTxt").innerHTML= fsad[film];
+        document.getElementById("ciboTxt").innerHTML= csad[cibo];
+        document.getElementById("libriTxt").innerHTML= lsad[libri];
+        document.getElementById("musicaTxt").innerHTML= msad[musica];
+        document.getElementById("serieTxt").innerHTML= ssad[serietv];
     break;
 
     case 's' :
-    break;
-
-    case 'S' :
+        fsurprise[film];
+        csurprise[cibo];
+        lsurprise[libri];
+        msurprise[musica];
+        ssurprise[serietv];
+        document.getElementById("filmTxt").innerHTML= fsurprise[film];
+        document.getElementById("ciboTxt").innerHTML= csurprise[cibo];
+        document.getElementById("libriTxt").innerHTML= lsurprise[libri];
+        document.getElementById("musicaTxt").innerHTML= msurprise[musica];
+        document.getElementById("serieTxt").innerHTML= ssurprise[serietv];
     break;
 }
